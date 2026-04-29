dac668ad3bd6f9f0ea9473a71bdc0dd5ffbd710b
Felipe-tt <felipeittnerc@gmail.com>
2026-04-29 16:33:30 +0000

    build: release

---
+++ app/src/main/java/com/hidrogas/app/DelegationService.java
@@ -0,0 +1,14 @@
+package com.hidrogas.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
